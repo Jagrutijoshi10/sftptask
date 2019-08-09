90ae4f4ca5f361b2cf1e404401e26520455e378e
Jagruti joshi <jvj1097@gmail.com>
2019-08-09 18:33:05 +0530

    logics changes

--- controller/controller.js
+++ controller/controller.js
@@ -1,15 +1,14 @@
 "use strict";
 let moment = require('moment');
 let _ = require('underscore');
-let configure=require('../config/config.json');
-let remotePathToList = '/home/joshi/sftpfolder';
-
+let configure = require('../config/config.json');
+let remotePathToList = '/home/joshi/sftpfolder/';
 let Client = require('ssh2').Client;
-// const Fs = require('fs')
+
 
 // TODO: move this to config.json with env as local and prod.eg: {local:{},prod:{}}
-module.exports= function() {
-    var self= {};
+module.exports = function () {
+    var self = {};
     self.getfiles = (req, res) => {
         let conn = new Client();
         conn.on('ready', function () {
@@ -19,76 +18,57 @@ module.exports= function() {
                     if (err) throw err;
                     // console.dir(list);
                     let finaldata = await calc(list);
-                    res.send(finaldata); 
+                    res.send(finaldata);
                     conn.end();
-                   
                     // console.log(finaldata) ;
                 });
             });
-        }).connect(configure.environment.local.connSettings);
+        }).connect(configure.sftp.local);
     }
     return self;
 }();
 
- function calc(log) {
+function calc(log) {
     //  log=log.attrs;
     //  console.log(log)
-     let isAbouve2hrs=false;
-    // let newarr = [];
-    let ct = moment().format('k:mm');
-    // console.log(ct)
-    let currentTime = moment.duration(ct).asMinutes();
+    let isAbouve2hrs = false;
+    let newarr = [];
+    let currentTime = moment().format('LLL');
     // console.log(currentTime);
     _.each(log, (i) => {
         try {
-            let at = moment.unix(i.attrs.atime).format('k:mm');
-            console.log(at);
-            let age = moment.duration(at).asMinutes();
+            let age = moment.unix(i.attrs.atime).format('LLL');
             // console.log(age);
             if (age <= currentTime) {
-                var longAge = moment(currentTime).diff(age);
+                var longAge = moment(currentTime).diff(age, 'minutes');
             }
             else {
-                var longAge = moment(age).diff(currentTime);
+                var longAge = moment(age).diff(currentTime, 'minutes');
             }
             // console.log(longAge);
             if (longAge >= 120) {
-                isAbouve2hrs=true;
-                let ct = "currentTime";
-                let value = currentTime + ' min';
-                i.attrs[ct] = value;
-                let tt = "longAge";
-                let value1 = longAge + ' min';
-                i.attrs[tt] = value1;
-                let flag="isAbouve2hrs";
-                let value2=isAbouve2hrs;
-                i.attrs[flag]=value2
+                isAbouve2hrs = true;
+                var data = _.extend({ i }, { "currentTime": currentTime, "age": age, "longAge": longAge + ' min', "isAbouve2hrs": isAbouve2hrs });
+                newarr.push(data);
                 // await newarr.push(i);
             }
             else {
-                isAbouve2hrs=false;
-                let tt = "longAge";
-                let value = longAge + ' min';
-                i.attrs[tt] = value;
-                let flag="isAbouve2hers";
-                let value2=isAbouve2hrs;
-                i.attrs[flag]=value2
-                // await newarr.push(i);
-                // console.log(i);
+                isAbouve2hrs = false;
+                var data = _.extend({ i }, { "currentTime": currentTime, "age": age, "longAge": longAge + ' min', "isAbouve2hrs": isAbouve2hrs });
+                newarr.push(data);
             }
             // await cb(null, list);
         }
         catch (e) {
             console.log(e);
         }
-
     })
     // console.log(newarr);
     if (log.length == 0) {
         var message = "No data found";
         return message;
     } else {
-        return  log;
+        return newarr;
     }
 
 };
--- test.js
+++ test.js
@@ -3,19 +3,39 @@
 const express = require('express'),
     test = express(),
     router1 = require('./router/router.js'),
-    router = express.Router();
+    testing = process.env.testing;
+    // console.log(process.env);
+    console.log("testing:",testing);
+
 test.use('/api', router1);
-test.listen((3000), () => {
-    console.log('server listening on port 3000');
+test.use((err,req,res,next) => {
+   if(err) throw err;
 });
+test.listen(3000)
 module.exports = test;
 
+// test.use('/api',cors,router1)
+
+// Enabled CORS
+
+// cors =new cors();
+// []
 
 
 
 
-// .on('unhandledRejection', (error) => {
-//     console.log(' The error was: ', error );
-//   }),
+// try{
+//     if(!err){
+//         console.log('server listening on port 3000');
+//     }
+//    else{
+//        throw new err;
+//    }
+// }
+// catch(err){
+//    console.log("error caught");
+//    return err;
+
+// }
 // TODO: need to catch errors and ensure server never crashes
 //Question: CORS how to implement using this logics? any example would help
